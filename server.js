3ad8d045706f0354a9d3df946eff3a9aa092c0be
Daniel Mayorga <daniel.mayorga97@hotmail.com>
2023-09-30 17:25:21 -0400

    deployment commit

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-const express = require('./config/express');
-const app = express();
+    process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+    const express = require('./config/express');
+    const PORT = process.env.PORT || 8080;
+    const app = express();
 
-app.listen(5000, () => {
-    console.log('Server running at http://localhost:5000/');
-});
+    app.listen(PORT, () => {
+        console.log('Server running at http://localhost:5000/');
+    });
 
-module.exports = app;
+    module.exports = app;
